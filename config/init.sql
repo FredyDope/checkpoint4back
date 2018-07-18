2e73b4386c651ff765813181c3f974fffe644e80
frederic <fredydope@gmail.com>
2018-07-18 12:52:26 +0200

    add ligne de commande sql pour les routes get put post delete update

--- config/init.sql
+++ config/init.sql
@@ -21,14 +21,37 @@ USE `movies` ;
 -- Table `movies`.`movieDetail`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `movies`.`movieDetail` (
-  `id` INT NOT NULL,
-  `Titre` VARCHAR(45) NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Titre` VARCHAR(45) NULL, 
   `Sortie` DATE NULL,
   `Genre` VARCHAR(45) NULL,
   `Synopsys` TEXT(500) NULL,
   `Affiche` VARCHAR(2000) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
++----------+---------------+------+-----+---------+-------+
+| Field    | Type          | Null | Key | Default | Extra |
++----------+---------------+------+-----+---------+-------+
+| id       | int(11)       | NO   | PRI | NULL    |       |
+| Titre    | varchar(45)   | YES  |     | NULL    |       |
+| Sortie   | date          | YES  |     | NULL    |       |
+| Genre    | varchar(45)   | YES  |     | NULL    |       |
+| Synopsys | text          | YES  |     | NULL    |       |
+| Affiche  | varchar(2000) | YES  |     | NULL    |       |
++----------+---------------+------+-----+---------+-------+
+
+SELECT * FROM movieDetail 
+
+SELECT * FROM movieDetail WHERE id =${id}
+
+INSERT INTO movieDetail (Titre, Sortie, Genre, Synopsys, Affiche)
+VALUES ('Blade Runner', '1982-09-15', 'Science Fiction', 'Après avoir massacré un équipage et pris le contrôle d\'un vaisseau, quatre androïdes ultra perfectionnés parviennent à s\'introduire dans Los Angeles. Rick Deckard, un agent spécial aussi appelé Blade Runner, est chargé de les éliminer. Officiellement, on ne parle pas d\'exécution, mais de retrait.', 'https://media.senscritique.com/media/000005695693/source_big/Blade_Runner.jpg')
+
+UPDATE movieDetail SET Titre = 'Blade Runner', Sortie = '1982-09-15', Genre = 'Science Fiction', Synopsys = 'près avoir massacré un équipage et pris le contrôle d\'un vaisseau, quatre androïdes ultra perfectionnés parviennent à s\'introduire dans Los Angeles. Rick Deckard, un agent spécial aussi appelé Blade Runner, est chargé de les éliminer. Officiellement, on ne parle pas d\'exécution, mais de retrait.', Affiche = 'https://media.senscritique.com/media/000005695693/source_big/Blade_Runner.jpg'
+WHERE id = ${id}
+
+DELETE FROM movieDetail WHERE id =${id}
 
 
 -- -----------------------------------------------------
